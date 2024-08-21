6de3b4c7be14757d03d0ecddebc4e347d42481e9
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-08-21 19:03:18 -0300

    update script init

--- init.sql
+++ init.sql
@@ -6,5 +6,4 @@ INSERT INTO usuarios (id, nombre,apellido,email,username, password) VALUES
 (1, 'admin','admin','admin@gmail.com','admin', '$2a$12$De8WtfX53AyrD9orwSFWHeJyqyt8er5iQ3Nt071juOwUK84/T8F2m');
 
 INSERT INTO user_roles(user_id,role_id) VALUES
-(1,2),
 (1,3);
