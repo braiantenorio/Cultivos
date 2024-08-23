cf8d7326d16f1f7464cd11705d16d49141ec8520
Braian Tenorio <tenoriovidalbraian@gmail.com>
2024-08-22 21:44:19 -0300

    update lpl e init script

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
-INSERT INTO roles(name) VALUES('ROLE_USER');
-INSERT INTO roles(name) VALUES('ROLE_MODERATOR');
-INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+INSERT INTO roles(id,name) VALUES(1,'ROLE_USER');
+INSERT INTO roles(id,name) VALUES(2,'ROLE_MODERATOR');
+INSERT INTO roles(id,name) VALUES(3,'ROLE_ADMIN');
 
 INSERT INTO usuarios (id, nombre,apellido,email,username, password) VALUES
 (1, 'admin','admin','admin@gmail.com','admin', '$2a$12$De8WtfX53AyrD9orwSFWHeJyqyt8er5iQ3Nt071juOwUK84/T8F2m');
